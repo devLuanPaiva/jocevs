344b50b6c727d7058a482c64604264f2b1923ae7
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-21 14:44:58 -0300

    feat: export team modules

---
+++ packages/core/src/teams/index.ts
@@ -0,0 +1,8 @@
+import { ETypeTeam } from "./model/EType.enum";
+import { ITeam } from "./model/ITeam.interface";
+import { ECategoryTeam } from "./model/ECaterogy.enum";
+import { ITeamsRepository } from "./model/ITeamRepository.interface";
+import Team from "./model/Team.class";
+
+export type { ECategoryTeam, ITeam, ITeamsRepository, ETypeTeam };
+export { Team };
