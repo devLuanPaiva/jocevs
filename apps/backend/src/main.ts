157b5c1b8f37502a8e3a55cbb3e2f7e59136e172
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-21 16:16:37 -0300

    chore: configure main enable cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, POST, PUT, HEAD, PATCH, DELETE',
+    credentials: true,
+  });
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
