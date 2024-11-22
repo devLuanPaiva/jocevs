e172ce86056770c911944c6228ca6f50e6bd56a1
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-22 11:07:58 -0300

    feat: add new attributes

--- packages/core/src/games/model/IGames.interface.ts
+++ packages/core/src/games/model/IGames.interface.ts
@@ -1,13 +1,16 @@
+import { ECategoryTeam, ETypeTeam } from "../../teams";
 import { ITeam } from "../../teams/model/ITeam.interface";
 import { ELocation } from "./ELocation.enum";
 import { EStateGame } from "./EState.enum";
 
-export interface IGames{
-    id: number;
-    team1: ITeam;
-    team2: ITeam;
-    date: Date;
-    time: number;
-    location: ELocation;
-    state: EStateGame
+export interface IGames {
+  id: number;
+  team1: ITeam;
+  team2: ITeam;
+  date: Date;
+  time: number;
+  location: ELocation;
+  state: EStateGame;
+  category: ECategoryTeam;
+  type: ETypeTeam;
 }
