94546ff9c1e21e26cc760b1550b411f9a8aabf26
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-22 11:04:01 -0300

    feat: add new verifications

--- packages/core/src/games/model/Game.class.ts
+++ packages/core/src/games/model/Game.class.ts
@@ -1,11 +1,12 @@
 import { ECategoryTeam } from "../../teams/model/ECaterogy.enum";
 import { ETypeTeam } from "../../teams/model/EType.enum";
+import { EStateGame } from "./EState.enum";
 import { IGames } from "./IGames.interface";
 import { IGamesRepository } from "./IGamesRepository.interface";
 
 export default class Game {
   constructor(private readonly repo: IGamesRepository) {}
-  async addGame(game: Partial<IGames>) {
+  async addGame(game: IGames) {
     const games = await this.repo.getGames();
     const hasGameInLocationAndTime = games.find(
       (g) => g.location === game.location && g.time === game.time
@@ -16,7 +17,24 @@ export default class Game {
     if (game.team1 === game.team2) {
       throw new Error("O time 1 e o time 2 não podem ser o mesmo time");
     }
-    return this.repo.addGame(game);
+    if (game.team1.category !== game.team2.category) {
+      throw new Error("Os times devem ter categorias iguais");
+    }
+    if (game.team1.type !== game.team2.type) {
+      throw new Error("Os times devem ter tipos iguais");
+    }
+    if (game.category !== (game.team1.category && game.team2.category)) {
+      throw new Error("Os times devem ter a mesma categoria do jogo");
+    }
+    if (game.type !== (game.team1.type && game.team2.type)) {
+      throw new Error("Os times devem ter o mesmo tipo do jogo");
+    }
+
+    const newGame: IGames = {
+      ...game,
+      state: EStateGame.WAITING,
+    };
+    return this.repo.addGame(newGame);
   }
   async getGames(): Promise<IGames[]> {
     return this.repo.getGames();
@@ -25,17 +43,17 @@ export default class Game {
     return this.repo.getGameById(id);
   }
 
-  async getGamesFromCategory(category: ECategoryTeam): Promise<IGames[]> {
-    return this.repo.getGamesFromCategory(category);
+  async getGamesByCategory(category: ECategoryTeam): Promise<IGames[]> {
+    return this.repo.getGamesByCategory(category);
   }
-  async getGamesFromType(type: ETypeTeam): Promise<IGames[]> {
-    return this.repo.getGamesFromType(type);
+  async getGamesByType(type: ETypeTeam): Promise<IGames[]> {
+    return this.repo.getGamesByType(type);
   }
-  async getGamesFromDate(date: Date): Promise<IGames[]> {
-    return this.repo.getGamesFromDate(date);
+  async getGamesByDate(date: Date): Promise<IGames[]> {
+    return this.repo.getGamesByDate(date);
   }
-  async getGamesFromTeam(teamId: number): Promise<IGames[]> {
-    return this.repo.getGamesFromTeam(teamId);
+  async getGamesByTeam(teamId: number): Promise<IGames[]> {
+    return this.repo.getGamesByTeam(teamId);
   }
   async updateGame(id: number, updatedGame: Partial<IGames>): Promise<void> {
     const games = await this.repo.getGames();
@@ -49,5 +67,11 @@ export default class Game {
       throw new Error("O time 1 e o time 2 não podem ser o mesmo time");
     }
     return this.repo.updateGame(id, updatedGame);
+  }
+  async updateState(id: number, state: EStateGame): Promise<void> {
+    return this.repo.updateState(id, state);
+  }
+  async deleteGame(id: number): Promise<void> {
+    return this.repo.deleteGame(id);
   }
 }
