3b92d03b38b539b1a9614ed905813162fde71a95
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-22 11:09:20 -0300

    feat: import gamer module in app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './db/prisma.module';
 import { TeamModule } from './team/TeamModule.module';
+import { GamerModule } from './games/GamerModule.module';
 
 @Module({
-  imports: [PrismaModule, TeamModule],
+  imports: [PrismaModule, TeamModule, GamerModule],
   controllers: [AppController],
   providers: [],
 })
